db8f7b63e8e8a159a9cd0ae9a273d05b0e0a32ba
Jakub Narloch <jmnarloch@gmail.com>
2012-04-05 20:33:40 +0200

    Initial test suite.

---
+++ src/test/java/org/jboss/arquillian/spring/testsuite/test/NullEmployeeRepositoryTestCase.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spring.testsuite.test;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.spring.testsuite.beans.Employee;
+import org.jboss.arquillian.spring.testsuite.beans.repository.EmployeeRepository;
+import org.jboss.arquillian.spring.testsuite.beans.repository.impl.DefaultEmployeeRepository;
+import org.jboss.arquillian.spring.testsuite.beans.repository.impl.NullEmployeeRepository;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNull;
+
+/**
+ * Tests the {@link NullEmployeeRepository} class.
+ *
+ * @author <a href="mailto:jmnarloch@gmail.com">Jakub Narloch</a>
+ */
+@RunWith(Arquillian.class)
+public class NullEmployeeRepositoryTestCase {
+
+    @Deployment
+    public static JavaArchive createTestArchive() {
+        return ShrinkWrap.create(JavaArchive.class, "spring-test.jar")
+                .addClasses(Employee.class,
+                        EmployeeRepository.class, DefaultEmployeeRepository.class, NullEmployeeRepository.class)
+                .addAsResource(NullEmployeeRepositoryTestCase.class.getResource("/applicationContext.xml"),
+                        "applicationContext.xml");
+    }
+
+    @Autowired
+    @Qualifier("nullEmployeeRepository")
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void testGetEmployees() throws Exception {
+
+        List<Employee> result = employeeRepository.getEmployees();
+
+        assertNull("Null was expected.", result);
+    }
+}
